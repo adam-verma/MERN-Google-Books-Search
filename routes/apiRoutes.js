caa5ec98b133da5ca88a61f0648f85348d2ff4e7
Adam Verma <vermaadam@gmail.com>
2020-02-28 22:13:48 -0600

    Added necessary routes into apiRoutes.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+// Defining routes for sending to database
+const router = require("express").Router();
+const bookController = require("../controllers/bookController");
+
+// For all books, match them with route "api/books"
+router.route("/")
+  .get(bookController.findAll)
+  .post(bookController.create);
+
+  // For a book specific with id, match it with route "/api/books/:id"
+  router.route('/:id')
+    .delete(bookController.remove);
+
+    module.exports = router; 
